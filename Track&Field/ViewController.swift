207e9ec2e329ee7ed9be105b8d39a5643cadb31d
ibukiSano <ibusan2005@gmail.com>
2022-06-05 18:00:09 +0900

    履歴トップ

--- Track&Field/ViewController.swift
+++ Track&Field/ViewController.swift
@@ -13,14 +13,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-//bfuo
-    //watched
-    //watched too
-    //keok
-    //rmdjkdk
-    //koskokoks
-    
     
 }
 
