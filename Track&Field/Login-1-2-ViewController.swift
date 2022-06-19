c0ffe872afbe75df6c672904fd4b56a7fab1a982
ibukiSano <ibusan2005@gmail.com>
2022-06-19 19:49:53 +0900

    6/19 sano teaching

---
+++ Track&Field/Login-1-2-ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  Login-1-2-ViewController.swift
+//  Track&Field
+//
+//  Created by 佐野生樹 on 2022/06/19.
+//
+
+import UIKit
+
+class Login_1_2_ViewController: UIViewController {
+
+    @IBOutlet weak var email_TF: UITextField!
+    @IBOutlet weak var username_TF: UITextField!
+    @IBOutlet weak var password_TF: UITextField!
+    @IBOutlet weak var repassword_TF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func register_tapped() {
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
