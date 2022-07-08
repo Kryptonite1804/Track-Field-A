eea0207bb4dfa4444e131257fcfd2f2ab5e6b1b7
ibukiSano <ibusan2005@gmail.com>
2022-07-08 21:10:59 +0900

    sano 7/8 viewcontroller

---
+++ Track&Field/History-1-ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  History-1-ViewController.swift
+//  Track&Field
+//
+//  Created by 佐野生樹 on 2022/07/08.
+//
+
+import UIKit
+
+class History_1_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
