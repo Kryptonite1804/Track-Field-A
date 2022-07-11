cd01fd483b4701eb11776a6523dcb215e016ca96
ibukiSano <ibusan2005@gmail.com>
2022-07-11 12:28:16 +0900

    7/11 会議

--- Track&Field/Record-0-ViewController.swift
+++ Track&Field/Record-0-ViewController.swift
@@ -28,10 +28,67 @@ class Record_0_ViewController: UIViewController {
     
     @IBOutlet weak var writing: UITextView!
     
+    @IBOutlet weak var mealButton: UIButton!
+    @IBOutlet weak var tiredRevelButton: UIButton!
+    @IBOutlet weak var placeTypeButton: UIButton!
     
+    var aboutButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+                
+                let body_Array = [["練習タイプ","excerciseType"],["チーム","team"],["練習場所タイプ","placeType"]]
+                let excerciseType_Array = ["JOG","LSD","集団走","スピード練","大会","記録会","補強","その他"]
+                let team_Array = ["A","B","C"]
+                let placeType_Array = ["トラック","ロード","校庭","その他"]
+                
+                
+                var detail_Array = [String]()
+                for m in 0 ... body_Array.count - 1 {
+                    let bodyJP_String = body_Array[m][0]
+                    let bodyEN_String = body_Array[m][1]
+                    
+                    if m == 0 {
+                        aboutButton = mealButton
+                        detail_Array = excerciseType_Array
+                    } else if m == 1 {
+                        
+                        aboutButton = tiredRevelButton
+                        detail_Array = team_Array
+                    } else if m == 2 {
+                        aboutButton = placeTypeButton
+                        detail_Array = placeType_Array
+                    }
+                    
+                    
+                    
+                    
+                    
+                    aboutButton.setTitle(bodyJP_String, for: .normal)
+                    
+                    var action_Array = [UIMenuElement]()
+                    
+                    for n in 0...detail_Array.count - 1 {
+                        
+                        let detail_String = detail_Array[n] //excerciseType_Arrayからひとつ取り出し ex."JOG"
+                        
+                        let action = UIAction(title: "\(detail_String)", image: UIImage(systemName: "pencil"), handler: { _ in //タイトル設定
+                            print("\(bodyEN_String): \(detail_String)") //何が選択されたか表示
+                            self.aboutButton.setTitle("\(detail_String)", for: .normal) //選択されたものをButtonのTitleに
+                        })
+                        
+                        action_Array.append(action)  //上記一連の動作を設定
+                        
+                    }
+                    
+                        let items = UIMenu(options: .displayInline, children: action_Array) //ここでリストの全ボタンを配置
+                    aboutButton.menu = UIMenu(title: "\(bodyJP_String)を選択", children: [items]) //リスト最上部に出てくる小さくて薄い文字
+                    aboutButton.showsMenuAsPrimaryAction = true //アニメーションさせる
+                    
+                    
+                    
+                }
 
         // Do any additional setup after loading the view.
     }
